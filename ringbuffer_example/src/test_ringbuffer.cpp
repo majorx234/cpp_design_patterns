c6a42881e568381752dc4f98164f511cb630d982
MajorX234 <majorx234@googlemail.com>
2022-04-20 16:14:29 +0200

    Add different fillings of the ringe buffer, check for edge cases

--- ringbuffer_example/src/test_ringbuffer.cpp
+++ ringbuffer_example/src/test_ringbuffer.cpp
@@ -32,6 +32,34 @@ int main(int argc, char const *argv[])
     else
       std::cout << "empty" << std::endl;
     test_ring_buffer->pop(); 
-  }  
+  }
+
+  for (int i = 20; i < 30; ++i)
+  {
+    test_ring_buffer->push(i); 
+  }
+
+  for (int i = 0; i < 11; ++i)
+  {
+    if(auto value = test_ring_buffer->top())
+      std::cout << "value: " << *value << std::endl;
+    else
+      std::cout << "empty" << std::endl;
+    test_ring_buffer->pop(); 
+  }
+
+  for (int i = 30; i < 40; ++i)
+  {
+    test_ring_buffer->push(i); 
+  }
+
+  for (int i = 0; i < 11; ++i)
+  {
+    if(auto value = test_ring_buffer->top())
+      std::cout << "value: " << *value << std::endl;
+    else
+      std::cout << "empty" << std::endl;
+    test_ring_buffer->pop(); 
+  }
   return 0;
 }
