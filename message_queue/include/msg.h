01af249a859c9aaeb31274e30cae1ef8d04f17a4
MajorX234 <majorx234@googlemail.com>
2022-05-04 00:11:42 +0200

    Add unique id for Msgs objects

--- message_queue/include/msg.h
+++ message_queue/include/msg.h
@@ -9,7 +9,10 @@ public:
   Msg();
   ~Msg();
   virtual std::unique_ptr<Msg> move();
+  unsigned long long get_unique_id() const;
   
   unsigned char data[26];
+ private:  
+  unsigned long long unique_Id_;
 };
 #endif // MSG_H_
--- message_queue/src/msg.cpp
+++ message_queue/src/msg.cpp
@@ -1,4 +1,21 @@
 #include "msg.h"
+#include <atomic>
+
+unsigned long long generate_unique_id()
+{
+    static std::atomic<unsigned long long> i(0);
+    return ++i;
+}
+
+Msg::Msg()
+  : unique_Id_(generate_unique_id())
+{
+
+}
+
+Msg::~Msg(){
+    
+}
 
 std::unique_ptr<Msg> Msg::move()
 {
