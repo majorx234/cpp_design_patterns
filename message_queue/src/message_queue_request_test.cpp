38898f8d061163ea657b8c3dbc9eae72d649b8c1
MajorX234 <majorx234@googlemail.com>
2022-05-09 00:00:31 +0200

    Add implementation of test thread functions

--- message_queue/src/message_queue_request_test.cpp
+++ message_queue/src/message_queue_request_test.cpp
@@ -7,15 +7,23 @@
 
 std::atomic<bool> alive = true;
 
-
-void server_function(MessageQueue &my_queue, std::promise<Request::MsgID> &prom) {
+void server_function(MessageQueue *my_queue, std::promise<Request::MsgID> *prom) {
   while (alive) {
+    auto future = prom->get_future();
+    Request::MsgID id = future.get();
+    auto msg = std::make_unique<Msg>("test");
+    my_queue->respond_to_request(id, msg->move());
     std::this_thread::sleep_for(std::chrono::milliseconds(1200));
   }
 }
 
-void client_function(MessageQueue &my_queue, std::promise<Request::MsgID> &prom) {
+void client_function(MessageQueue *my_queue, std::promise<Request::MsgID> *prom) {
   while (alive) {
+    prom->set_value(Request::MsgID::msg_type1);
+    auto msg_opt = my_queue->register_request(Request::MsgID::msg_type1,1500);
+    if (msg_opt) {
+      std::cout << msg_opt.value()->data << std::endl;
+    }
     std::this_thread::sleep_for(std::chrono::milliseconds(1200));
   }  
 }
@@ -23,11 +31,13 @@ void client_function(MessageQueue &my_queue, std::promise<Request::MsgID> &prom)
 int main(){
   MessageQueue my_queue;
   std::promise<Request::MsgID> prom_msg_id;
-  std::thread server_thread(server_function, my_queue, prom_msg_id);
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  std::thread client_thread(client_function, my_queue, prom_msg_id);
 
+  std::thread client_thread(client_function, &my_queue, &prom_msg_id);  
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  std::thread server_thread(server_function, &my_queue, &prom_msg_id);
   std::this_thread::sleep_for(std::chrono::seconds(6));
+  
   alive = false;
   client_thread.join();
   server_thread.join();
